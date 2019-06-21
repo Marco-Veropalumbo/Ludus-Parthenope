4a0b145aab2f40b7b58c5862e2ac1e84802478c6
Marco-Veropalumbo <51475652+Marco-Veropalumbo@users.noreply.github.com>
2019-06-22 01:11:44 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, session, request, redirect
+from flask import Flask, render_template, url_for, session, request, redirect, flash
 from flask_pymongo import PyMongo
 import bcrypt
 
@@ -22,27 +22,40 @@ def login():
     if login_user:
         if bcrypt.hashpw(request.form['pw'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
+            try:
+                session['team'] = login_user['team']
+            except:
+                pass
             return redirect(url_for('index'))
-    return 'Username o password errati'
+    else:
+        flash("Username o password errati")
+        return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
-   session.pop('username', None)
-   return redirect(url_for('index'))
+    session.pop('team', None)
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route('/register', methods=['POST','GET'])
 def register():
     if request.method == 'POST':
         users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username'] })
+        existing_user = users.find_one({'name' : request.form['username']})
+        existing_email = users.find_one({'email' : request.form['email']})
 
         if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pw'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
+            if existing_email is None:
+                hashpass = bcrypt.hashpw(request.form['pw'].encode('utf-8'), bcrypt.gensalt())
+                users.insert({'name' : request.form['username'], 'password' : hashpass, 'email' : request.form['email']})
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+            else:
+                flash("Questa email è già stata registrata da un altro utente")
+                return redirect(url_for('register'))
         else:
-            return 'Esiste gi&agrave; un utente con questo username!'
+            flash("Esiste già un utente con questo username!")
+            return redirect(url_for('register'))
 
     return render_template('register.html')
 
@@ -72,9 +85,96 @@ def join(torneo):
 @app.route('/aggiungi',methods=['POST'])
 def aggiungi():
     newgame = request.form['add']
-    mongo.db.users.update_one({'name': session['username'] }, {"$addToSet": {'games': newgame}})
-    return redirect(url_for('library'))
+    user_library = mongo.db.users.find_one({"$and":[{'name': session['username']},{'games': newgame}]})
+    if user_library:
+        flash("Gioco già presente in libreria")
+        return redirect(url_for('library'))
+    else:
+        mongo.db.users.update_one({'name': session['username'] }, {"$addToSet": {'games': newgame}})
+        return redirect(url_for('library'))
 
+@app.route('/search',methods=['POST'])
+def search():
+    risultato = mongo.db.tournaments.find_one({'name': request.form['codice']})
+    if risultato:
+        session.ricerca = risultato
+        return render_template('search.html')
+    else:
+        flash("Torneo non trovato")
+        return redirect(url_for('home'))
+
+@app.route('/admin')
+def admin():
+    if session['username'] == "admin":
+        session.database = mongo.db
+        return render_template('admin.html')
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/addgame',methods=['POST'])
+def addgame():
+    if mongo.db.games.find_one({'title': request.form['newgame']}):
+        flash("Gioco già presente")
+        return redirect(url_for('admin'))
+    else:
+        mongo.db.games.insert({'title': request.form['newgame']})
+        flash("Gioco inserito con successo")
+        return redirect(url_for('admin'))
+
+@app.route('/addtournament',methods=['POST'])
+def addtournament():
+    if mongo.db.tournaments.find_one({'name': request.form['newtournamentid']}):
+        flash("Torneo già presente")
+        return redirect(url_for('admin'))
+    else:
+        date= request.form['newtournamentdate'] + " " + request.form['newtournamenthour']
+        mongo.db.tournaments.insert({'name': request.form['newtournamentid'], 'title': request.form['newtournamentgame'], 'date': date})
+        flash("Torneo inserito con successo")
+        return redirect(url_for('admin'))
+
+@app.route('/team')
+def team():
+    if 'username' in session:
+        session.database = mongo.db
+        return render_template('team.html')
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/searchteam',methods=['POST'])
+def searchteam():
+    risultato = mongo.db.teams.find_one({'name': request.form['teamname']})
+    if risultato:
+        mongo.db.users.update_one({'name': session['username']},{"$set": {'team': request.form['teamname']}})
+        mongo.db.teams.update_one({'name': request.form['teamname']},{"$addToSet":{'users': session['username']}})
+        session['team'] = request.form['teamname']
+        return redirect(url_for('team'))
+
+    else:
+        flash("Il team inserito non è stato trovato")
+        return redirect(url_for('team'))
+
+@app.route('/createteam',methods=['POST'])
+def createteam():
+    risultato = mongo.db.teams.find_one({'name': request.form['teamname']})
+    if risultato:
+        flash("Esiste già un team con questo nome")
+        return redirect(url_for('team'))
+    else:
+        mongo.db.teams.insert({'name': request.form['teamname']})
+        mongo.db.users.update_one({'name': session['username']}, {"$set": {'team': request.form['teamname']}})
+        mongo.db.teams.update_one({'name': request.form['teamname']}, {"$addToSet": {'users': session['username']}})
+        session['team'] = request.form['teamname']
+        return redirect(url_for('team'))
+
+@app.route('/leaveteam')
+def leaveteam():
+    mongo.db.users.update_one({'name': session['username']}, {"$unset": {'team': session['team']}})
+    mongo.db.teams.update_one({'name': session['team']}, {"$pull": {'users': session['username']}})
+    numero_membri = mongo.db.teams.find_one({'name': session['team']})
+    if len(numero_membri['users']) == 0:
+        mongo.db.teams.delete_one({'name': session['team']})
+    session.pop('team',None)
+    return redirect(url_for('team'))
 
 if __name__ == '__main__':
     app.run()
